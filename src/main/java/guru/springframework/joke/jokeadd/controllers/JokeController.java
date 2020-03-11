9d9f24e7b71f13a6335776bb2faa4decc0e37cad
Liubomyr680 <lshynkaruk8@gmail.com>
2020-03-11 10:28:57 +0200

    Add Controller

--- src/main/java/guru/springframework/joke/jokeadd/controllers/JokeController.java
+++ src/main/java/guru/springframework/joke/jokeadd/controllers/JokeController.java
@@ -1,5 +1,6 @@
-package guru.springframework.joke.jokeadd.services;
+package guru.springframework.joke.jokeadd.controllers;
 
+import guru.springframework.joke.jokeadd.services.JokeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;

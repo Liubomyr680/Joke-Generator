d7a3bb18848d65425d6fd53adad073311593db51
Liubomyr680 <lshynkaruk8@gmail.com>
2020-03-11 10:03:15 +0200

    Create RandomJokeGenerator

---
+++ src/test/java/guru/springframework/joke/jokeadd/JokeaddApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.joke.jokeadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeaddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
